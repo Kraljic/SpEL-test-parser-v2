1a2cef057aeb111b0dea5a94b5e0a259f6bd0f08
kraljic <marko.kraljic10@gmail.com>
2021-01-16 13:38:39 +0100

    Initial commit

---
+++ src/main/java/hr/kraljic/spel/textparser2/TextParser2Application.java
@@ -0,0 +1,13 @@
+package hr.kraljic.spel.textparser2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextParser2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextParser2Application.class, args);
+    }
+
+}
