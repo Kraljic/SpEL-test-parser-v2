785600b6121d2cc84a0a8a2c8da63e3619f50848
kraljic <marko.kraljic10@gmail.com>
2021-01-17 11:23:38 +0100

    Javadoc

--- src/main/java/hr/kraljic/spel/textparser2/parser/config/ExceptionHandler.java
+++ src/main/java/hr/kraljic/spel/textparser2/parser/config/ExceptionHandler.java
@@ -1,9 +1,25 @@
 package hr.kraljic.spel.textparser2.parser.config;
 
+/**
+ * Defines how will SpEL expression be handled in case of exception.
+ */
 public enum ExceptionHandler {
+    /**
+     * The exception will be rethrown and the process will terminate.
+     */
     THROW_EXCEPTION,
+    /**
+     * Expression will be retained, if expression is in sub context every parent context will try to parse expression.
+     * <p>Use this handler if u want to share parent context in sub context
+     */
     RETAIN_EXPRESSION,
+    /**
+     * Expression will be replaced with single whitespace.
+     */
     PRINT_WHITESPACE,
+    /**
+     * Expression will be ignored (removed from text).
+     */
     PRINT_EMPTY_STRING;
 
     public static final ExceptionHandler DEFAULT = ExceptionHandler.RETAIN_EXPRESSION;
--- src/main/java/hr/kraljic/spel/textparser2/parser/config/NullHandler.java
+++ src/main/java/hr/kraljic/spel/textparser2/parser/config/NullHandler.java
@@ -1,8 +1,17 @@
 package hr.kraljic.spel.textparser2.parser.config;
 
 public enum NullHandler {
+    /**
+     * Nulls will be replace with single white space
+     */
     PRINT_WHITE_SPACE(" "),
+    /**
+     * Nulls wont be printed at all
+     */
     PRINT_EMPTY_STRING(""),
+    /**
+     * Nulls will be printed as "null"
+     */
     PRINT_NULL("null"),
     ;
     public static final NullHandler DEFAULT = PRINT_NULL;
